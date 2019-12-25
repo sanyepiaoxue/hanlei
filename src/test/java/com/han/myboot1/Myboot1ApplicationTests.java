d836142fcee818f0e73d742296b19e854b214ccd
hanlei <1490477430@qq.com>
2019-12-25 19:39:48 +0800

    a

---
+++ src/test/java/com/han/myboot1/Myboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.han.myboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myboot1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
